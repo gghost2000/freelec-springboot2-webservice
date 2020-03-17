c6a803951bf5219352ff4da787cd4d29b7bc7913
jihwan.hyun <gghost2000@naver.com>
2020-03-17 23:40:10 +0900

    add postsave and TestCode

---
+++ src/main/java/com/jojoidu/admin/service/PostsService.java
@@ -0,0 +1,18 @@
+package com.jojoidu.admin.service;
+
+import com.jojoidu.admin.domain.posts.PostsRepository;
+import com.jojoidu.admin.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
