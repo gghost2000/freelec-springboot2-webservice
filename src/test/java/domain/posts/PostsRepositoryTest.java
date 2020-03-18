422028e758adc1e5695e21d2a7e93fdc76b05d7a
jihwan.hyun <gghost2000@naver.com>
2020-03-18 22:54:05 +0900

    add index.mutache ,

--- src/test/java/domain/posts/PostsRepositoryTest.java
+++ src/test/java/domain/posts/PostsRepositoryTest.java
@@ -23,26 +23,46 @@ public class PostsRepositoryTest {
     PostsRepository postsRepository;
 
     @After
-    public void  cleanup(){
+    public void cleanup() {
         postsRepository.deleteAll();
     }
 
     @Test
-    public void postSave(){
+    public void postSave() {
         String title = "테스트 게시글";
         String content = "테스트 본문";
 
         postsRepository.save(Posts.builder()
-        .title(title)
-        .content(content)
-        .author("jojoldu@gmail.com")
-        .build());
+                .title(title)
+                .content(content)
+                .author("jojoldu@gmail.com")
+                .build());
 
         List<Posts> postsList = postsRepository.findAll();
 
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntityEnroll() {
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>> createDate=" + posts.getCreatedDate()+ ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
     }
 
 }
