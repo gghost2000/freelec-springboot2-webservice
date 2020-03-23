e161d5781441467349c85244a1b7330a55b160f8
jihwan.hyun <gghost2000@naver.com>
2020-03-23 23:54:57 +0900

    fronend

--- src/main/java/com/jojoidu/admin/service/PostsService.java
+++ src/main/java/com/jojoidu/admin/service/PostsService.java
@@ -2,12 +2,16 @@ package com.jojoidu.admin.service;
 
 import com.jojoidu.admin.domain.posts.Posts;
 import com.jojoidu.admin.domain.posts.PostsRepository;
+import com.jojoidu.admin.web.dto.PostsListResponseDto;
 import com.jojoidu.admin.web.dto.PostsResponseDto;
 import com.jojoidu.admin.web.dto.PostsSaveRequestDto;
 import com.jojoidu.admin.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -25,10 +29,25 @@ public class PostsService {
         posts.update(requestDto.getTitle(),requestDto.getContent());
         return id;
     }
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
+        postsRepository.delete(posts);
+    }
+    @Transactional(readOnly = true)
     public PostsResponseDto findById (Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(() ->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
 }
 
--- src/main/java/com/jojoidu/admin/web/PostsApiController.java
+++ src/main/java/com/jojoidu/admin/web/PostsApiController.java
@@ -1,11 +1,14 @@
 package com.jojoidu.admin.web;
 
 import com.jojoidu.admin.service.PostsService;
+import com.jojoidu.admin.web.dto.PostsListResponseDto;
 import com.jojoidu.admin.web.dto.PostsResponseDto;
 import com.jojoidu.admin.web.dto.PostsSaveRequestDto;
 import com.jojoidu.admin.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,11 +22,22 @@ public class PostsApiController {
 
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
-        return postsService.update(id,requestDto);
+        return postsService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 
     @GetMapping("/api/v1/posts/{id}")
-    public PostsResponseDto findById(@PathVariable Long id){
+    public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
     }
 }
