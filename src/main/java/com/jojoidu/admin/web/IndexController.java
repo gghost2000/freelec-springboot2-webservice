33462bad32bdcb82a9fb0de0c59b132e3a8b3f42
jihwan.hyun <gghost2000@naver.com>
2020-04-03 19:11:45 +0900

    add naver Login and security test

--- src/main/java/com/jojoidu/admin/web/IndexController.java
+++ src/main/java/com/jojoidu/admin/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoidu.admin.web;
 
+import com.jojoidu.admin.config.auth.LoginUser;
 import com.jojoidu.admin.config.auth.dto.SessionUser;
 import com.jojoidu.admin.service.PostsService;
 import com.jojoidu.admin.web.dto.PostsResponseDto;
@@ -18,9 +19,8 @@ public class IndexController {
     private final PostsService postsService;
     private final HttpSession httpSession;
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
